49b2c830e6e734d87edae8676fa4fe4f2af5fd41
neonknight <github@neonknight.ch>
2018-01-05 13:40:28 +0100

    fix jabber tls compatibility

--- src/monsta/notification/jabber.py
+++ src/monsta/notification/jabber.py
@@ -1,6 +1,43 @@
 from monsta.notification import Messenger
 import logging
 import time
+import ssl
+try:
+    import xmpp
+    XMPP_AVAILABLE = True
+except ImportError:
+    XMPP_AVAILABLE = False
+
+
+
+# monkey patch broken TLS
+def _startSSL(self):
+    """ Immidiatedly switch socket to TLS mode. Used internally."""
+    """ Here we should switch pending_data to hint mode."""
+    tcpsock=self._owner.Connection
+    # noinspection PyProtectedMember
+    tcpsock._sslObj    = ssl.wrap_socket(tcpsock._sock, None, None)
+    # noinspection PyProtectedMember
+    tcpsock._sslIssuer = tcpsock._sslObj.getpeercert().get('issuer')
+    # noinspection PyProtectedMember
+    tcpsock._sslServer = tcpsock._sslObj.getpeercert().get('server')
+    # noinspection PyProtectedMember
+    tcpsock._recv = tcpsock._sslObj.read
+    # noinspection PyProtectedMember
+    tcpsock._send = tcpsock._sslObj.write
+
+    tcpsock._seen_data=1
+    self._tcpsock=tcpsock
+    tcpsock.pending_data=self.pending_data
+    # noinspection PyProtectedMember
+    tcpsock._sock.setblocking(0)
+
+    self.starttls='success'
+
+if XMPP_AVAILABLE:
+    xmpp.transports.TLS._startSSL = _startSSL
+
+
 
 class JabberMessenger(Messenger):
     """Notifications over JABBER/XMPP/Google Talk"""
@@ -15,9 +52,7 @@ class JabberMessenger(Messenger):
         self.helpstrings['recipient']='alert recipient jid'
         
     def lint(self):
-        try:
-            import xmpp
-        except ImportError:
+        if not XMPP_AVAILABLE:
             print "You are trying to use the jabber messenger, but the python xmpp lib (xmpppy) is not installed."
             return False
             
@@ -37,8 +72,6 @@ class JabberMessenger(Messenger):
     
      
     def send_message(self,message,subject=None):
-        import xmpp
-        
         jid=xmpp.protocol.JID(self.configvars['jid'])
         cl=xmpp.Client(jid.getDomain(),debug=[])
         con=cl.connect()
